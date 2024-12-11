19d7888d24d5ab00ab363b30cebd986137918459
arinochkaaa <153714566+arinochkaaa@users.noreply.github.com>
2024-12-12 00:13:12 +0300

    Add files via upload

---
+++ lab12/OOP_Lab12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace OOP_Lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Тест метода GetDiskInfo()");
+                VAVDiskInfo.GetDiskInfo();
+                Console.WriteLine("\n\nТест метода GetFileInfo()");
+                VAVFileInfo.GetFileInfo("D:\\3 семм\\ООП\\12_Потоки_файловая система.pdf");
+                Console.WriteLine("\n\nТест метода GetDirInfo()");
+                VAVDirInfo.GetDirInfo("D:\\3 семм\\ООП");
+                Console.WriteLine("\n\nТест метода GetFilesAndFoulders()");
+                VAVFileManager.GetFilesAndFoulders("..\\net6.0");
+                Console.WriteLine("\n\nТест метода CreateCopyOfFile()");
+                VAVFileManager.CreateCopyOfFile("..\\net6.0\\VAVInspect\\VAVdirinfo.txt");
+                Console.WriteLine("\n\nТест метода Remove()");
+                //VAVFileManager.Remove("..\\net6.0\\VAVInspect\\VAVdirinfo.txt\\copy_TDSdirinfo.txt");
+                Console.WriteLine("\n\nСоздание директория VAVFiles");
+                VAVFileManager.CreateDirectory("..\\net6.0", "VAVFiles");
+                Console.WriteLine("\n\nКопирование файлов с расширением .dll и .exe в директорию VAVFiles");
+                VAVFileManager.ReplaceTo("..\\net6.0", "VAVFiles", ".dll", ".exe");
+                Console.WriteLine("\n\nУпаковка в zip-архив VAVFiles");
+                VAVFileManager.CreateZip("VAVFiles");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ lab12/OOP_Lab12/VAVDirInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace OOP_Lab12
+{
+    static class VAVDirInfo
+    {
+        public static void GetDirInfo(string path)//про папку ООП
+        {
+            DirectoryInfo dirInf = new(path);
+            if (dirInf.Exists)
+            {
+                Console.WriteLine("Имя каталога: {0}", dirInf.Name);//тут у нас имя
+                Console.WriteLine("Полный путь: {0}", dirInf.FullName);// тут полный путь
+                Console.WriteLine("Время создания: {0}", dirInf.CreationTime);//тут как будто дальше все понятно
+                Console.WriteLine("Корневой каталог: {0}", dirInf.Root);
+                Console.WriteLine("Родительский каталог: {0}", dirInf.Parent);
+                Console.WriteLine("Количество файлов: {0}", dirInf.GetFiles().Length);
+                Console.WriteLine("Количество подкаталогов: {0}", dirInf.GetDirectories().Length);
+            }
+            VAVLog.WriteToFile(MethodBase.GetCurrentMethod().Name, MethodBase.GetCurrentMethod().DeclaringType.Name);
+        }
+    }
+}
